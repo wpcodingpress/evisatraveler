bffc14d338c89a1b456fa9415e9d673a018aecd3
DeployBot <deploy@evisatraveler.com>
2026-04-27 13:59:37 +0600

    fix: remove white space gap above footer

--- src/components/PublicLayout.tsx
+++ src/components/PublicLayout.tsx
@@ -38,7 +38,7 @@ export default function PublicLayout({ children }: { children: React.ReactNode }
     <>
       <Header />
       {mounted && <MobileMenuRoot user={user} />}
-      {children}
+      <main className="flex-1 min-h-screen">{children}</main>
       <Footer />
     </>
   );
